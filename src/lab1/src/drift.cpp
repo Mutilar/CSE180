c21734e8a3f85df5e25164ef9ca1a2f0cf90a884
Brian <brianhungerman@gmail.com>
2019-02-12 18:06:58 -0800

    Lab1 finished/commented

--- src/lab1/src/drift.cpp
+++ src/lab1/src/drift.cpp
@@ -1,20 +1,56 @@
+/*
+ *	BRIAN HUNGERMAN
+ *  ROBOTICS, CSE 180
+ *  FEB 12, 2019
+ */
+
 #include <ros/ros.h>
-#include <std_msgs/Int32.h>
-#include <std_msgs/String.h>
+#include <geometry_msgs/Pose.h>
+#include <nav_msgs/Odometry.h>
+#include <std_msgs/Float64.h>
 
-void intMessageReceived(const std_msgs::Int32&msg) {
-  ROS_INFO_STREAM("Received integer"<<msg.data);
-}
-void  stringMessageReceived(const std_msgs::String&msg) {
-  ROS_INFO_STREAM("Received string" <<msg.data);
-}
+//Store local copies of most recent poses
+geometry_msgs::Pose odomPos;
+geometry_msgs::Pose basePos;
 
-int main (int argc, char** argv)
+//Store local copy of return value
+std_msgs::Float64 val;
+
+//Publisher for /posedrift
+ros::Publisher pubDist;
+
+//On receiving Odometry reading
+void odomReceived(const nav_msgs::Odometry &msg)
 {
-	ros::init(argc, argv , "listener") ;
-	ros::NodeHandle  nh ;
+	//Update local copy of Odometry reading
+	odomPos = msg.pose.pose;
+	//If ready to publish
+	if (ros::ok())
+	{
+		//Calculate distance betwee real and theoretical XY positions
+		double x1 = odomPos.position.x, x2 = basePos.position.x, y1 = odomPos.position.y, y2 = basePos.position.y;
+		val.data = sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+		//Push calculated distance to /posedrift
+		pubDist.publish(val);
+	}
+}
 
-	ros::Subscriber subInt = nh.subscribe("integers",1000,&intMessageReceived);
-	ros::Subscriber subString = nh.subscribe("strings",1000,&stringMessageReceived);
+//On Receiving Pose Ground Truth reading
+void baseReceived(const nav_msgs::Odometry &msg)
+{
+	//Update local copy of Pose Ground Truth
+	basePos = msg.pose.pose;
+}
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "drifty-drift");
+	ros::NodeHandle nh;
+	//Initialize Subscriptions
+	ros::Subscriber subOdom = nh.subscribe("/pioneer/odom", 1000, &odomReceived);
+	ros::Subscriber subActual = nh.subscribe("/base_pose_ground_truth", 1000, &baseReceived);
+	//Initialize Publisher
+	pubDist = nh.advertise<std_msgs::Float64>("/posedrift", 1000);
+
 	ros::spin();
 }
--- src/lab1/src/moverobot.cpp
+++ src/lab1/src/moverobot.cpp
@@ -1,24 +1,32 @@
+/*
+ *	BRIAN HUNGERMAN
+ *  ROBOTICS, CSE 180
+ *  FEB 12, 2019
+ */
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 
-int main (int argc, char** argv)
+int main(int argc, char **argv)
 {
-	ros::init(argc, argv , "chatter") ;
-	ros::NodeHandle  nh ;
-	ros::Publisher pubVel = nh.advertise<geometry_msgs::Twist>("/pioneer/cmd_vel",1000);
-	ros::Rate rate(1); //1 command per second
-	
+	ros::init(argc, argv, "movey-move");
+	ros::NodeHandle nh;
+	ros::Publisher pubVel = nh.advertise<geometry_msgs::Twist>("/pioneer/cmd_vel", 1000);
+	//1 command per second (more of a "square" with a value of ~.4)
+	ros::Rate rate(1);
+	//Boolean to alternate between driving forward and turning
 	bool isTurning = false;
 
 	while (ros::ok())
 	{
-	  isTurning = !isTurning;
+		isTurning = !isTurning;
 
-	  geometry_msgs::Twist msg;
-	  if (isTurning) { msg.angular.z = .5; }
-	  else { msg.linear.x = 1; }
-	  
-	  pubVel.publish(msg);	
-	  rate.sleep();
+		geometry_msgs::Twist msg;
+		//rotate .5 rad/s
+		if (isTurning) msg.angular.z = .5;
+		//move 1 m/s forward
+		else msg.linear.x = 1;
+		//send message
+		pubVel.publish(msg);
+		rate.sleep();
 	}
 }
