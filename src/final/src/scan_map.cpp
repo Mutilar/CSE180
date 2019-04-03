5934612c8325e98183053fe04060e55ce31fcf01
Brian Hungerman <brianhungerman@gmail.com>
2019-04-03 16:08:49 -0700

    init nodes

---
+++ src/final/src/scan_map.cpp
@@ -0,0 +1,22 @@
+/*
+ *  ROBOTICS, CSE 180
+ */
+
+#include <ros/ros.h>
+#include <math.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_listener.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_msgs/TFMessage.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/Twist.h>
+
+int main(int argc, char **argv)
+{
+
+	ros::init(argc, argv, "scan");
+	ros::NodeHandle nh;
+
+  //...
+
+}
