d0b873fbe1298866a7fadf5996d2fac9eca062eb
Brian Hungerman <brianhungerman@gmail.com>
2019-04-04 15:00:33 -0700

    adding navigation_map.cpp

---
+++ src/final/src/navigate_map.cpp
@@ -0,0 +1,98 @@
+/*
+ *  ROBOTICS, CSE 180
+ */
+
+#include <ros/ros.h>
+#include <math.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_listener.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_msgs/TFMessage.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/Twist.h>
+
+#include <actionlib/client/simple_action_client.h>
+#include <move_base_msgs/MoveBaseAction.h>
+
+using namespace std;
+
+void setGoal();
+
+/* Start movement on receiving commands */
+bool has_been_instructed = false;
+
+move_base_msgs::MoveBaseGoal goal;
+
+/* Sets of two, target x, target y for robot to move towards (output of node) */
+vector<float> target_points;
+
+
+
+
+void getPoints(const geometry_msgs::Vector3& point) {
+	std::cout << "RECEIVING POINT(" << point.x << ", " << point.y << ")\n";
+
+	target_points.push_back(point.x);
+	target_points.push_back(point.y);
+
+	has_been_instructed = true;
+}
+
+void setGoal() {
+
+	// ROS_INFO_STREAM("TARGET(" << target_x << ", " << target_y << ")");
+    actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> ac("move_base");
+	
+    cout << "waiting for server...\n";
+
+	while (!ac.waitForServer()) { }
+
+	cout << "found server...\n";
+	
+	goal.target_pose.header.frame_id = "map";
+	goal.target_pose.header.stamp = ros::Time::now();
+	
+	cout << "TARGET( y = " << target_points.back() << ", ";
+	goal.target_pose.pose.position.y = target_points.back(); 
+	target_points.pop_back();
+	cout << "x = " << target_points.back() << ")\n";
+	goal.target_pose.pose.position.x = target_points.back();
+	target_points.pop_back();
+
+
+	goal.target_pose.pose.orientation.w = 1.0;
+
+	ac.sendGoal(goal);
+	ac.waitForResult();
+
+	std::cout << target_points.size() << "\n";
+
+	if (target_points.size() == 0) {
+		ros::shutdown();
+	}
+	//
+	setGoal();
+}
+
+int main(int argc, char **argv)
+{
+
+	ros::init(argc, argv, "navigate");
+	ros::NodeHandle nh;
+
+	ros::Subscriber subscribe_points = nh.subscribe("/target_points", 1000, &getPoints);
+
+ 	while (has_been_instructed == false) {
+ 		ros::Duration(.01).sleep();	
+
+		ros::spinOnce();
+	}
+	cout << "received instructions\n\n";
+
+	ros::Duration(1).sleep();	
+
+	setGoal();
+	ros::spin();
+
+	return 0;
+}
