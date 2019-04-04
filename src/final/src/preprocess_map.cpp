e031812d4f2dbd404c9294709a2b2923708fad5d
Brian Hungerman <brianhungerman@gmail.com>
2019-04-03 19:32:33 -0700

    Map->Matrix + Box Filter for smoothing

--- src/final/src/preprocess_map.cpp
+++ src/final/src/preprocess_map.cpp
@@ -12,6 +12,10 @@
 #include <tf2_msgs/TFMessage.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <geometry_msgs/Twist.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
 
 /* Map Metadata Constants*/
 float meters_per_pixel;
@@ -25,42 +29,82 @@ int map_x_min, map_x_max, map_y_min, map_y_max;
 
 void getMap(const nav_msgs::OccupancyGrid &map)
 {
+	/* Reading Metadata */
 	meters_per_pixel = map.info.resolution;
 	width = map.info.width;
 	height = map.info.height;
 
-
-
-	map_matrix = new bool*[height];
-	for (int i = 0; i < height; i++) {
-		map_matrix[i] = new bool[width];
-	} 
-
-	for (int i = 0; i < width * height; i++) {
-		std::cout << "READING POINT(" << i % width << ", " << i / width << ") = " << map.data[i]*1.0 << ";\n";		
-		map_matrix[i % width][i / width] = map.data[i] == 0;
-	} 
-
+	/* Defining Relevant Boundaries (given 20m x 20m space) */
 	map_x_min = (width / 2) - (10 / meters_per_pixel);
 	map_x_max = (width / 2) + (10 / meters_per_pixel);
 
 	map_y_min = (height / 2) - (10 / meters_per_pixel);
 	map_y_max = (height / 2) + (10 / meters_per_pixel);
 
+	/* Initializing && Populating Map Matrix */
+	map_matrix = new bool*[height];
+	for (int i = 0; i < height; i++) map_matrix[i] = new bool[width];
+	for (int i = 0; i < width * height; i++) map_matrix[i / width][i % width] = map.data[i] == 0;	
 
-	// bool is_all_inaccessible = true;
-	// for (map_y_min = 0; map_y_min < height && is_all_inaccessible == true; map_y_min++) {
-	// 	for (int j = 0; j < width; j++) {
-	// 		if (map.data[map_y_min + j * width] != -1) {
-	// 			is_all_inaccessible = false;
-	// 			break;
-	// 		}
-	// 	}
-	// } 
-	std::cout << "Bounding(" << map_y_min << "," << map_y_max << "\n";
+	/* Applying Box Filter to Matrix to smooth inperfections in scan */
+	for (int num_passes = 0; num_passes < 1; num_passes++) {
+		for (int x = map_x_min; x < map_x_max; x++) {
+			for (int y = map_y_min; y < map_y_max; y++) {
+				int num_available_space = 0;
+				if (map_matrix[x+1][y+0]) num_available_space++;
+				if (map_matrix[x+0][y+1]) num_available_space++;
+				if (map_matrix[x+1][y+1]) num_available_space++;
+
+				if (map_matrix[x-1][y-0]) num_available_space++;
+				if (map_matrix[x-0][y-1]) num_available_space++;
+				if (map_matrix[x-1][y-1]) num_available_space++;
+
+				if (map_matrix[x+1][y-1]) num_available_space++;
+				if (map_matrix[x-1][y+1]) num_available_space++;
+				if (num_available_space >= 5) {
+					map_matrix[x+0][y+0] = true;
+				}
+				if (num_available_space <= 3) {
+					map_matrix[x+0][y+0] = false;
+				}
+			}	
+		}
+	}
+	
+	/* Defining "end cap" detection iterator */
+
+	//	1 	1 	1 	1 	1 	1 	1 	1 	1 	1
+	//  1 	1 	1 	1 	1 	1 	1 	1 	1 	1
+	//  1 	? 	? 	? 	? 	? 	? 	? 	? 	1
+	//  1 	? 	? 	0 	0 	0 	0 	? 	? 	1
+	//  1 	? 	0 	0 	0 	0 	0 	0 	? 	1
+
+	//Scan for a match with iterator
+
+		//if match, continue setting 0s in current direction until hits black...
+
+	//rotate iterator by 90, repeat x 3
 
 
 
+
+
+	ofstream myfile;
+  	myfile.open ("plot.txt");
+  	if (myfile.is_open())
+  	{
+
+		for (int x = map_x_min; x < map_x_max; x++) {
+			for (int y = map_y_min; y < map_y_max; y++) {
+				myfile << map_matrix[x][y] << " ";
+				std::cout << map_matrix[x][y] << " ";
+			}
+			myfile << "\n";
+			std::cout << "\n";
+		}
+	 }
+  	else cout << "Unable to open file";
+	myfile.close();
 
 	std::cout << meters_per_pixel << "\n";
 
