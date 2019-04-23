ab3188accbdbc738c6cff0e349389d3e28710554
Brian Hungerman <brianhungerman@gmail.com>
2019-04-23 08:24:09 -0700

    Lab8 zipped

--- build/final/CMakeFiles/scan_map.dir/cmake_clean.cmake
+++ build/final/CMakeFiles/scan_map.dir/cmake_clean.cmake
@@ -1,10 +1,5 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/scan_map.dir/src/scan_map.cpp.o"
-  "/home/mutilar/CSE180/devel/lib/final/scan_map.pdb"
-  "/home/mutilar/CSE180/devel/lib/final/scan_map"
-)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang )
   include(CMakeFiles/scan_map.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
---
+++ build/lab8/CMakeFiles/move.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/move.dir/src/move.cpp.o"
+  "/home/mutilar/CSE180/devel/lib/lab8/move.pdb"
+  "/home/mutilar/CSE180/devel/lib/lab8/move"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/move.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
